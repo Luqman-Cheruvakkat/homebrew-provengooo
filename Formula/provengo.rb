a4cddf5fd876c29da8a4e0aee3c23ff183cfe007
luqmanpolus <luqman.c@polussolutions.com>
2025-05-18 16:34:30 +0530

    updated formula

--- Formula/provengo.rb
+++ Formula/provengo.rb
@@ -6,7 +6,8 @@ class Provengo < Formula
 
   def check_java_version
     java_version = `java -version 2>&1 | awk -F '"' '/version/ {print $2}'`.chomp
-    java_major = java_version.split(".").first.to_i
+    java_major = (java_version.split(".").then { |parts| parts[0] == "1" ? parts[1].to_i : parts[0].to_i })
+
 
     if java_major < 11
       odie "Error: Java 11 or higher is required. Detected version: #{java_version}"
